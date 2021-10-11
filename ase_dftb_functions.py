b3646916237529f79e20137849463dbf29c91efa
Shay Chaudhuri <57219048+shaychaudhuri@users.noreply.github.com>
2021-10-11 17:54:55 +0100

    Update ase_dftb_functions.py

--- ase_dftb_functions.py
+++ ase_dftb_functions.py
@@ -6,3 +6,24 @@ import os
 
 ## Kelvin-Hartree conversion factor
 K2Ha = 0.316681534524639E-05
+
+## Define DFTB+ optimisation calculator
+def dftb_opt_calc(atoms):
+  calc = Dftb(
+    # Driver settings
+    Driver_ = 'LBFGS',
+    Driver_AppendGeometries = 'YES',
+    Driver_LatticeOpt = 'NO',
+    Driver_MaxSteps = -1, 
+    
+    # SCC settings
+    Hamiltonian_SCC = 'YES',
+    Hamiltonian_MaxSCCIterations = 3000,
+    Hamiltonian_SCCTolerance = 1E-5, 
+    Hamiltonian_ShellResolvedSCC = 'YES',
+    
+    # Hamiltonian-specific settings
+    Hamiltonian_Filling_ = 'Fermi',
+    Hamiltonian_Filling_Temperature = 1160*K2Ha,
+    
+    
