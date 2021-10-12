f814793215bb2819dabccd99aa26d84ae6c8420b
Shay Chaudhuri <57219048+shaychaudhuri@users.noreply.github.com>
2021-10-12 13:46:06 +0100

    Update ase_dftb_functions.py

--- ase_dftb_functions.py
+++ ase_dftb_functions.py
@@ -37,3 +37,67 @@ def dftb_opt_calc(atoms):
     kpts = (16,16,1) if (atoms.get_pbc() == [True, True, True]).all() else None,
     
     # Dispersion settings
+    Hamiltonian_Dispersion_ = 'MBD',
+    Hamiltonian_Dispersion_KGrid = '1 1 1',
+    Hamiltonian_Dispersion_Beta = 0.95,
+    
+    # Analysis settings
+    Analysis_ = '',
+    Analysis_CalculateForces = 'YES',
+    
+    # Options
+    Options_ = '',
+    Options_WriteResultsTag = 'YES',
+    Options_WriteDetailedOut = 'YES',
+    Options_WriteChargesAsText = 'YES',
+    Options_TimingVerbosity = -1)
+  
+  # Set maximal azimuthal quantum numbers
+  set_max_angular_momenta(calc, atoms)
+  
+  return calc
+    
+## Define DFTB+ single-point calculator
+def dftb_opt_calc(atoms):
+  calc = Dftb(
+    # SCC settings
+    Hamiltonian_SCC = 'YES',
+    Hamiltonian_MaxSCCIterations = 3000,
+    Hamiltonian_SCCTolerance = 1E-5, 
+    Hamiltonian_ShellResolvedSCC = 'YES',
+    
+    # Hamiltonian-specific settings
+    Hamiltonian_Filling_ = 'Fermi',
+    Hamiltonian_Filling_Temperature = 1160*K2Ha,
+    Hamiltonian_Mixer_ = 'Broyden',
+    Hamiltonian_Mixer_MixingParameter = 0.01,
+    Hamiltonian_Mixer_InverseJacobiWeight = 0.01,
+    Hamiltonian_Mixer_MinimalWeight = 1.0,
+    Hamiltonian_Mixer_MaximalWeight = 1E5,
+    
+    Hamiltonian_SlaterKosterFiles_Prefix = 'dev/null',
+    Hamiltonian_MaxAngularMomentum_ = '',
+    
+    kpts = (16,16,1) if (atoms.get_pbc() == [True, True, True]).all() else None,
+    
+    # Analysis settings
+    Analysis_ = '',
+    Analysis_CalculateForces = 'YES',
+    
+    # Options
+    Options_ = '',
+    Options_WriteResultsTag = 'YES',
+    Options_WriteDetailedOut = 'YES',
+    Options_WriteChargesAsText = 'YES',
+    Options_TimingVerbosity = -1)
+  
+  # Set maximal azimuthal quantum numbers
+  set_max_angular_momenta(calc, atoms)
+  
+  return calc
+
+def set_max_angular_momenta(calc, atoms):
+  max_ls = {'H': '"s"', 'C': '"p"', 'N': '"p"', 'O': '"p"', 'Au': '"d"'}
+  sym = [chemical_symbols[i] for i in np.unique(atoms.get_atomic_numbers())]
+  settings = {f'Hamiltonian_MaxAngularMomentum_{s}': max_ls[s] for s in sym}
+  calc.parameters.update(settings)
