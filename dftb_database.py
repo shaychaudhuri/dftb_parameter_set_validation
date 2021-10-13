60aa766ec8279e23d601761a03cfc8175c9cdc2d
Shay Chaudhuri <57219048+shaychaudhuri@users.noreply.github.com>
2021-10-13 17:04:18 +0100

    Update dftb_database.py

--- dftb_database.py
+++ dftb_database.py
@@ -13,47 +13,47 @@ aims_db = connect("FHIaims.db")
 dftb_db = connect(f'{parameter_set}.db')
 
 for i in range(len(aims_db)):
-  os.mkdir(aims_db[i].data.name)
-  os.chdir(aims_db[i].data.name)
+    os.mkdir(aims_db[i].data.name)
+    os.chdir(aims_db[i].data.name)
   
-  ## Run optimisation calculation
-  atoms = read(f'FHIaims.db@{i-1}')
-  dftb_opt = dftb_opt_calc(atoms)
-  dftb_opt.set(Hamiltonian_SlaterKosterFiles_Prefix = f"path/to/{parameter_set}/")
-  if (atoms.get_pbc() == [True, True, True]).all():
-    dftb_opt.set(Hamiltonian_Dispersion_KGrid = '16 16 1')
-  else:
-    dftb_opt.set(Hamiltonian_Dispersion_KGrid = '1 1 1')
+    ## Run optimisation calculation
+    atoms = read(f'FHIaims.db@{i-1}')
+    dftb_opt = dftb_opt_calc(atoms)
+    dftb_opt.set(Hamiltonian_SlaterKosterFiles_Prefix = f"path/to/{parameter_set}/")
+    if (atoms.get_pbc() == [True, True, True]).all():
+        dftb_opt.set(Hamiltonian_Dispersion_KGrid = '16 16 1')
+    else:
+        dftb_opt.set(Hamiltonian_Dispersion_KGrid = '1 1 1')
   
-  # Set constraints if needed
-  fixed = constrained_indices(atoms)
-  if len(fixed) != 0:
-    free = {i+1 for i in range(len(atoms)) if i not in fixed}   #i+1 as DFTB+ starts from 1, not 0
-    dftb_opt.set(Driver_MovedAtoms = free)
+    # Set constraints if needed
+    fixed = constrained_indices(atoms)
+    if len(fixed) != 0:
+        free = {i+1 for i in range(len(atoms)) if i not in fixed}   #i+1 as DFTB+ starts from 1, not 0
+        dftb_opt.set(Driver_MovedAtoms = free)
     
-  dftb_opt.calculate(atoms)
+    dftb_opt.calculate(atoms)
   
-  ## Run single-point calculation
-  os.mkdir('SinglePoint')
-  os.chdir('SinglePoint')
+    ## Run single-point calculation
+    os.mkdir('SinglePoint')
+    os.chdir('SinglePoint')
   
-  atoms = read("../geo_end.gen")
-  dftb_sp = dftb_sp_calc(atoms)
-  dftb_sp.set(Hamiltonian_SlaterKosterFiles_Prefix = f"path/to/{parameter_set}/")
+    atoms = read("../geo_end.gen")
+    dftb_sp = dftb_sp_calc(atoms)
+    dftb_sp.set(Hamiltonian_SlaterKosterFiles_Prefix = f"path/to/{parameter_set}/")
   
-  dftb_sp.calculate(atoms)
+    dftb_sp.calculate(atoms)
   
-  data = {'name': aims_db[i].data.name,
-          'dftb_out_opt': open("../dftb.out", 'r').read(),
-          'dftb_in_hsd_opt': open("../dftb_in.hsd", 'r').read(),
-          'dftb_out': open("dftb.out", 'r').read(),
-          'dftb_in_hsd': open("dftb_in.hsd", 'r').read(),
-          'dftb_pin_hsd': open("dftb_pin.hsd", 'r').read(),
-          'band_out': open("band.out", 'r').read(),
-          'charges_dat': open("charges.dat", 'r').read(),
-          'detailed_out': open("detailed.out", 'r').read()
-         }
+    data = {'name': aims_db[i].data.name,
+            'dftb_out_opt': open("../dftb.out", 'r').read(),
+            'dftb_in_hsd_opt': open("../dftb_in.hsd", 'r').read(),
+            'dftb_out': open("dftb.out", 'r').read(),
+            'dftb_in_hsd': open("dftb_in.hsd", 'r').read(),
+            'dftb_pin_hsd': open("dftb_pin.hsd", 'r').read(),
+            'band_out': open("band.out", 'r').read(),
+            'charges_dat': open("charges.dat", 'r').read(),
+            'detailed_out': open("detailed.out", 'r').read()
+           }
   
-  dftb_db.write(atoms, data)
+    dftb_db.write(atoms, data)
   
-  os.chdir('../../')
+    os.chdir('../../')
