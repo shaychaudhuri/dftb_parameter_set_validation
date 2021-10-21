034b70b2faf0a62614ceab0f3de3427659484717
Shay Chaudhuri <57219048+shaychaudhuri@users.noreply.github.com>
2021-10-21 12:16:41 +0100

    Update adsorption_energies.py

--- adsorption_energies.py
+++ adsorption_energies.py
@@ -16,8 +16,10 @@ size = len(dftb_db)+1
 dict = {'CH':'Methylidyne', 'CN':'Cyanide', 'CO':'Carbon_Monoxide', 'NO':'Nitric_Oxide'}
 orientation = ['top', 'bottom']
 
-## DFT adsorption energy
 for i in range(1, size):
+    ###########################
+    ## DFT adsorption energy ##
+    ###########################
     name = aims_db[i].data.name
     if '@' not in name:
         continue
@@ -31,7 +33,7 @@ for i in range(1, size):
         for line in combined_file:
             if "Total energy corr" in line:
                 linesplit = line.split()
-                combined_energy = float(linesplit[-2])
+                combined_energy_dft = float(linesplit[-2])
                 
         for j in range(1, size):
             ## Get substrate energy
@@ -40,7 +42,7 @@ for i in range(1, size):
                 for line in substrate_file:
                     if "Total energy corr" in line:
                         linesplit = line.split()
-                        substrate_energy = float(linesplit[-2])
+                        substrate_energy_dft = float(linesplit[-2])
                         
             ## Get adsorbate energy
             if adsorbate == aims_db[j].data.name:
@@ -48,7 +50,7 @@ for i in range(1, size):
                 for line in adsorbate_file:
                     if "Total energy corr" in line:
                         linesplit = line.split()
-                        adsorbate_energy = float(linesplit[-2])   
+                        adsorbate_energy_dft = float(linesplit[-2])   
             elif adsorbate in [f'{x}_{y}' for y in orientation for x in dict]:
                 symbols = adsorbate.split("_")[0]
                 adsorbate = dict[symbols]
@@ -57,12 +59,13 @@ for i in range(1, size):
                     for line in adsorbate_file:
                         if "Total energy corr" in line:
                             linesplit = line.split()
-                            adsorbate_energy = float(linesplit[-2])
+                            adsorbate_energy_dft = float(linesplit[-2])
                         
-        adsorption_energy = combined_energy - substrate_energy - adsorbate_energy
+        adsorption_energy_dft = combined_energy_dft - substrate_energy_dft - adsorbate_energy_dft
 
-## DFTB adsorption energy
-for i in range(1, size):
+    ############################    
+    ## DFTB adsorption energy ##
+    ############################
     name = dftb_db[i].data.name
     if '@' not in name:
         continue
@@ -76,7 +79,7 @@ for i in range(1, size):
         for line in combined_file:
             if "Total Energy:" in line:
                 linesplit = line.split()
-                combined_energy = float(linesplit[-2])
+                combined_energy_dftb = float(linesplit[-2])
                 
         for j in range(1, size):
             ## Get substrate energy
@@ -85,7 +88,7 @@ for i in range(1, size):
                 for line in substrate_file:
                     if "Total Energy:" in line:
                         linesplit = line.split()
-                        substrate_energy = float(linesplit[-2])
+                        substrate_energy_dftb = float(linesplit[-2])
                         
             ## Get adsorbate energy
             if adsorbate == dftb_sp_db[j].data.name:
@@ -93,7 +96,7 @@ for i in range(1, size):
                 for line in adsorbate_file:
                     if "Total Energy:" in line:
                         linesplit = line.split()
-                        adsorbate_energy = float(linesplit[-2])   
+                        adsorbate_energy_dftb = float(linesplit[-2])   
             elif adsorbate in [f'{x}_{y}' for y in orientation for x in dict]:
                 symbols = adsorbate.split("_")[0]
                 adsorbate = dict[symbols]
@@ -102,6 +105,6 @@ for i in range(1, size):
                     for line in adsorbate_file:
                         if "Total Energy:" in line:
                             linesplit = line.split()
-                            adsorbate_energy = float(linesplit[-2])
+                            adsorbate_energy_dftb = float(linesplit[-2])
                         
-        adsorption_energy = combined_energy - substrate_energy - adsorbate_energy                
+        adsorption_energy_dftb = combined_energy_dftb - substrate_energy_dftb - adsorbate_energy_dftb             
