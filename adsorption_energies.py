d0aaa7a60f1cf055b479ef926646b3af4376b03f
Shay Chaudhuri <57219048+shaychaudhuri@users.noreply.github.com>
2021-10-21 12:05:41 +0100

    Update adsorption_energies.py

--- adsorption_energies.py
+++ adsorption_energies.py
@@ -9,7 +9,7 @@ parameter_set = ''
 
 aims_db = connect("FHIaims.db")
 dftb_db = connect(f"{parameter_set}.db")
-dftb_sp_db = connect(f"{parameter_set}_SP.db")
+dftb_sp_db = connect(f"../{parameter_set}_SP/{parameter_set}_SP.db")
 
 size = len(dftb_db)+1
 
