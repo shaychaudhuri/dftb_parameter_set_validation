405f2da70d4dfe7557541250e98cd746ba7eba6c
Shay Chaudhuri <57219048+shaychaudhuri@users.noreply.github.com>
2021-10-21 11:45:39 +0100

    Update adsorption_energies.py

--- adsorption_energies.py
+++ adsorption_energies.py
@@ -9,6 +9,7 @@ parameter_set = ''
 
 aims_db = connect("FHIaims.db")
 dftb_db = connect(f"{parameter_set}.db")
+dftb_sp_db = connect(f"{parameter_set}_SP.db")
 
 size = len(dftb_db)+1
 
@@ -62,7 +63,7 @@ for i in range(1, size):
 
 ## DFTB adsorption energy
 for i in range(1, size):
-    name = dftb_sp_db[i].data.name
+    name = dftb_db[i].data.name
     if '@' not in name:
         continue
     else:
@@ -71,7 +72,7 @@ for i in range(1, size):
         adsorbate = name.split("@")[0]
         
         ## Get combined system energy
-        combined_file = dftb_sp_db[i].data.dftb_out.splitlines()
+        combined_file = dftb_db[i].data.dftb_out.splitlines()
         for line in combined_file:
             if "Total Energy:" in line:
                 linesplit = line.split()
