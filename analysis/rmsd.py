5aae9f52dd9356539ad06bc7d1fcfd8f7f1b0f5d
Shay Chaudhuri <57219048+shaychaudhuri@users.noreply.github.com>
2021-10-22 16:56:45 +0100

    Update rmsd.py

--- analysis/rmsd.py
+++ analysis/rmsd.py
@@ -11,3 +11,15 @@ dftb_db = connect(f'{parameter_set}.db')
 
 size = len(dftb_db)+1
 
+dict = {'CH':'Methylidyne', 'CN':'Cyanide', 'CO':'Carbon_Monoxide', 'NO':'Nitric_Oxide'}
+orientation = ['top', 'bottom']
+
+for i in range(1, size):
+    name = aims_db[i].data.name
+    dft_atoms = read('/path/to/FHIaims_database.db@'+str(i-1))
+    dft_positions = dft_atoms.get_positions()
+    
+    dftb_atoms = read(str(parameter_set)+'.db@'+str(i-1))
+    dftb_positions = dftb_atoms.get_positions()
+    
+    RMSD = np.average([(np.linalg.norm(dftb_positions[j]-dft_positions[j])**2) for j in range(len(dft_positions))])
